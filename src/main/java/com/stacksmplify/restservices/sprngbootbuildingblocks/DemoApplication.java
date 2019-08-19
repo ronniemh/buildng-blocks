79b4d13ed6ddd1d2d79e59259967c5604a914505
Ronnie Moncayo <me@ronniemoncayo.dev>
2019-08-18 19:14:55 -0500

    First master

--- src/main/java/com/stacksmplify/restservices/sprngbootbuildingblocks/DemoApplication.java
+++ src/main/java/com/stacksmplify/restservices/sprngbootbuildingblocks/DemoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Iniciando el proyecto! :D");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
