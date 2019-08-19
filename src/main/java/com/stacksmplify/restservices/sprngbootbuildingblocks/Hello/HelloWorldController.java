33967fbd26d6b59e9a75de962999152941f5b411
Ronnie Moncayo <me@ronniemoncayo.dev>
2019-08-18 19:55:11 -0500

    Hello World Base

--- src/main/java/com/stacksmplify/restservices/sprngbootbuildingblocks/Hello/HelloWorldController.java
+++ src/main/java/com/stacksmplify/restservices/sprngbootbuildingblocks/Hello/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.stacksmplify.restservices.sprngbootbuildingblocks.Hello;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-      /**
+    /**
      * Simple Method URI - /helloworld GET
      */
-    @RequestMapping(method = RequestMethod.GET, path = "/helloworld")
+    // @RequestMapping(method = RequestMethod.GET, path = "/helloworld")
+    @GetMapping("/helloworld1")
     public String helloWorld() {
 
         System.out.println("Entrando al método");
         return "Hello World final!";
+    }
+
+    @GetMapping("/helloworld-bean")
+    public UserDetails helloWorldBean(){
+        return new UserDetails("firstName", "lastName", "city");
     }
 }
