7c7b366cdbb993eeda17158423764ac828634e44
Ronnie Moncayo <rmoncayo@emagic.fin.ec>
2019-09-09 14:07:46 -0500

    I18n - first commit

--- src/main/java/com/stacksmplify/restservices/sprngbootbuildingblocks/Hello/HelloWorldController.java
+++ src/main/java/com/stacksmplify/restservices/sprngbootbuildingblocks/Hello/HelloWorldController.java
@@ -1,6 +1,12 @@
 package com.stacksmplify.restservices.sprngbootbuildingblocks.Hello;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,11 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
+    @Autowired
+    private ResourceBundleMessageSource messageSource;
     /**
      * Simple Method URI - /helloworld GET
      */
     // @RequestMapping(method = RequestMethod.GET, path = "/helloworld")
-    @GetMapping("/helloworld1")
+    @GetMapping("/helloworld")
     public String helloWorld() {
 
         System.out.println("Entrando al método");
@@ -23,5 +31,15 @@ public class HelloWorldController {
     @GetMapping("/helloworld-bean")
     public UserDetails helloWorldBean(){
         return new UserDetails("firstName", "lastName", "city");
+    }
+
+    @GetMapping("/hello-int")
+    public String getMessagesInI18nFormat(@RequestHeader(name = "Accept-Language", required = false) String locale){
+        return messageSource.getMessage("label.hello", null, new Locale(locale));
+    }
+
+    @GetMapping("/hello-int2")
+    public String getMessagesInI18nFormat2(){
+        return messageSource.getMessage("label.hello", null, LocaleContextHolder.getLocale());
     }
 }
