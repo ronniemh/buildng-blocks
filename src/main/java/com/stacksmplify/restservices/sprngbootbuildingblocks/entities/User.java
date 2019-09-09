4fe9903d70ed523071523d23ef72191acaf7440e
Ronnie Moncayo <rmoncayo@emagic.fin.ec>
2019-09-09 14:40:40 -0500

    Json Ignore - First commit

--- src/main/java/com/stacksmplify/restservices/sprngbootbuildingblocks/entities/User.java
+++ src/main/java/com/stacksmplify/restservices/sprngbootbuildingblocks/entities/User.java
@@ -11,6 +11,9 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import org.springframework.hateoas.ResourceSupport;
 
 /**
@@ -19,6 +22,7 @@ import org.springframework.hateoas.ResourceSupport;
 // @Entity(name = "nombre-real-db")
 @Entity
 @Table(name = "user")
+@JsonIgnoreProperties({"firstname", "lastname"})
 public class User extends ResourceSupport {
 
     @Id
@@ -43,6 +47,7 @@ public class User extends ResourceSupport {
     private String role;
 
     @Column(name = "SSN", length = 50, nullable = false, unique = true)
+    @JsonIgnore
     private String ssn;
 
     /**
