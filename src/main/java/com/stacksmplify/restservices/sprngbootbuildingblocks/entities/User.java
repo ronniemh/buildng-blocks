a2106452ce240343c0cd037997045704c06ce09f
Ronnie Moncayo <rmoncayo@emagic.fin.ec>
2019-09-13 11:34:18 -0500

    fix imports

--- src/main/java/com/stacksmplify/restservices/sprngbootbuildingblocks/entities/User.java
+++ src/main/java/com/stacksmplify/restservices/sprngbootbuildingblocks/entities/User.java
@@ -11,7 +11,6 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
-import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonView;
 
 import org.springframework.hateoas.ResourceSupport;
